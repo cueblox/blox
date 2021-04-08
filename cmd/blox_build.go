d48d19363ffc88ede530389d78d6146f09c349cb
Brian Ketelsen <bketelsen@gmail.com>
2021-04-08 09:43:24 -0400

    add errgroup

--- cmd/blox_build.go
+++ cmd/blox_build.go
@@ -14,6 +14,7 @@ import (
 	"github.com/hashicorp/go-multierror"
 	"github.com/pterm/pterm"
 	"github.com/spf13/cobra"
+	"golang.org/x/sync/errgroup"
 )
 
 var (
@@ -31,7 +32,7 @@ var buildCmd = &cobra.Command{
 		schemaDir, err := database.GetConfigString("schema_dir")
 		cobra.CheckErr(err)
 
-		err = filepath.WalkDir(schemaDir, func(path string, d fs.DirEntry, err error) error {
+		filepath.WalkDir(schemaDir, func(path string, d fs.DirEntry, err error) error {
 			if !d.IsDir() {
 				bb, err := os.ReadFile(path)
 				if err != nil {
@@ -79,75 +80,82 @@ func buildModels(db *cuedb.Database) error {
 	var errors error
 
 	pterm.Info.Println("Validating ...")
+	g := new(errgroup.Group)
 
-	for _, table := range db.GetTables() {
-		err := os.MkdirAll(db.GetTableDataDir(table), 0755)
-		if err != nil {
-			errors = multierror.Append(err)
-			continue
-		}
+	for _, tab := range db.GetTables() {
+		table := tab
+		g.Go(func() error {
 
-		err = filepath.Walk(db.GetTableDataDir(table),
-			func(path string, info os.FileInfo, err error) error {
-				if err != nil {
-					return err
-				}
+			err := os.MkdirAll(db.GetTableDataDir(table), 0755)
+			if err != nil {
+				errors = multierror.Append(err)
+				return err
+			}
 
-				if info.IsDir() {
-					return err
-				}
-
-				ext := strings.TrimPrefix(filepath.Ext(path), ".")
-
-				if !table.IsSupportedExtension(ext) {
-					return nil
-				}
-
-				slug := strings.TrimSuffix(filepath.Base(path), "."+ext)
-
-				bytes, err := ioutil.ReadFile(path)
-				if err != nil {
-					return multierror.Append(err)
-				}
-
-				// Loaders to get to YAML
-				// We should offer various, simple for now with markdown
-				mdStr := ""
-				if ext == "md" || ext == "mdx" {
-					mdStr, err = markdown.ToYAML(string(bytes))
+			err = filepath.Walk(db.GetTableDataDir(table),
+				func(path string, info os.FileInfo, err error) error {
 					if err != nil {
 						return err
 					}
 
-					bytes = []byte(mdStr)
-				}
+					if info.IsDir() {
+						return err
+					}
 
-				var istruct = make(map[string]interface{})
+					ext := strings.TrimPrefix(filepath.Ext(path), ".")
 
-				err = yaml.Unmarshal(bytes, &istruct)
+					if !table.IsSupportedExtension(ext) {
+						return nil
+					}
 
-				if err != nil {
-					return multierror.Append(err)
-				}
+					slug := strings.TrimSuffix(filepath.Base(path), "."+ext)
 
-				record := make(map[string]interface{})
-				record[slug] = istruct
+					bytes, err := ioutil.ReadFile(path)
+					if err != nil {
+						return multierror.Append(err)
+					}
 
-				err = db.Insert(table, record)
-				if err != nil {
-					return multierror.Append(err)
-				}
+					// Loaders to get to YAML
+					// We should offer various, simple for now with markdown
+					mdStr := ""
+					if ext == "md" || ext == "mdx" {
+						mdStr, err = markdown.ToYAML(string(bytes))
+						if err != nil {
+							return err
+						}
 
-				return err
+						bytes = []byte(mdStr)
+					}
 
-			},
-		)
+					var istruct = make(map[string]interface{})
 
-		if err != nil {
-			errors = multierror.Append(err)
-		}
+					err = yaml.Unmarshal(bytes, &istruct)
+
+					if err != nil {
+						return multierror.Append(err)
+					}
+
+					record := make(map[string]interface{})
+					record[slug] = istruct
+
+					err = db.Insert(table, record)
+					if err != nil {
+						return multierror.Append(err)
+					}
+
+					return err
+
+				},
+			)
+			return err
+		})
+		// Wait for all builds to complete.
+
 	}
-
+	err := g.Wait()
+	if err != nil {
+		errors = multierror.Append(err)
+	}
 	if errors != nil {
 		pterm.Error.Println("Validations failed")
 	} else {
--- go.mod
+++ go.mod
@@ -12,4 +12,5 @@ require (
 	github.com/spf13/cobra v1.1.3
 	github.com/spf13/viper v1.7.1
 	github.com/stretchr/testify v1.7.0
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c
 )
