ac44ec16e75e0581b21732a19e9b6982ba7b2da5
Brian Ketelsen <bketelsen@gmail.com>
2021-04-08 05:45:41 -0400

    add version to root

--- cmd/root.go
+++ cmd/root.go
@@ -13,11 +13,14 @@ import (
 
 var cfgFile string
 var quiet bool
+var Version string
+var BuildDate string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "drb",
-	Short: "A brief description of your application",
+	Use:     "blox",
+	Version: Version,
+	Short:   "A gitops tool for data and content",
 	Long: `A longer description that spans multiple lines and likely contains
 examples and usage of using your application. For example:
 
