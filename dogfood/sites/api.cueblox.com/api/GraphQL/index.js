8e8da05b8a07e46cc942de4c4a50fc707de330c4
Brian Ketelsen <bketelsen@gmail.com>
2021-04-09 13:56:01 -0400

    update URL

--- dogfood/sites/api.cueblox.com/api/GraphQL/index.js
+++ dogfood/sites/api.cueblox.com/api/GraphQL/index.js
@@ -3,7 +3,7 @@ const fetch = require('sync-fetch');
 const jsonGraphqlExpress = require('json-graphql-server').default;
 const createHandler = require("azure-function-express").createHandler;
 
-const data = fetch('https://github.com/bketelsen/bkml/releases/download/blox/data.json').json();
+const data = fetch('https://github.com/cueblox/blox/releases/download/blox/data.json').json();
 const app = require('express')();
 
 
