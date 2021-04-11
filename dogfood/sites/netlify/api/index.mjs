ea73c66396772cc036c56308a1fe919b0979a419
Brian Ketelsen <bketelsen@gmail.com>
2021-04-11 06:32:42 -0400

    mjs

--- dogfood/sites/netlify/api/index.mjs
+++ dogfood/sites/netlify/api/index.mjs
@@ -1,11 +1,13 @@
-const fetch = require("sync-fetch");
-const jsonGraphqlExpress = require("json-graphql-server").default;
+import express from 'express';
+import fetch from 'sync-fetch';
+import jsonGraphqlExpress from 'json-graphql-server';
+import serverless from 'serverless-http';
+
 const data = fetch(
   "https://github.com/cueblox/blox/releases/download/blox/data.json"
 ).json();
 
-const serverless = require("serverless-http");
-const app = require("express")();
+const app = express();
 
 const functionName = "serverless-http";
 
