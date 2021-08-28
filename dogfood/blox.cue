63b46c9e7e063217d74f155f3ab11bbedb7df664
Brian Ketelsen <bketelsen@gmail.com>
2021-08-28 06:57:16 -0400

    plugins are looking better

--- dogfood/blox.cue
+++ dogfood/blox.cue
@@ -3,4 +3,8 @@
   schemata_dir: "schemata"
   template_dir: "data/tpl"
   static_dir: "static"
+  prebuild: [ {
+    name: "images"
+    executable: "../images_impl"
+  }]
 }
---
+++ plugins/shared/pre.go
@@ -0,0 +1,13 @@
+package shared
+
+import "github.com/hashicorp/go-plugin"
+
+// PrebuildHandshakeConfigs are used to just do a basic handshake between
+// a plugin and host. If the handshake fails, a user friendly error is shown.
+// This prevents users from executing bad plugins or executing a plugin
+// directory. It is a UX feature, not a security feature.
+var PrebuildHandshakeConfig = plugin.HandshakeConfig{
+	ProtocolVersion:  1,
+	MagicCookieKey:   "BLOX_PLUGIN",
+	MagicCookieValue: "prebuild",
+}
